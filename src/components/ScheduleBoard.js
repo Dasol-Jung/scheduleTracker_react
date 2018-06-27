37d846aa028902ed455d1fc994ca503dab5f00e3
Dasol-Jung <xenosophian@gmail.com>
2018-06-27 21:04:57 +0900

    added a table element with a colgroup, a thead, a tbody inside

--- src/components/ScheduleBoard.js
+++ src/components/ScheduleBoard.js
@@ -5,7 +5,18 @@ class ScheduleBoard extends React.Component{
     render(){
         return(
             <div>
+                <table>
+                    <colgroup>
+                        <col className='col1' span='3'/>
+                        <col className='col2' span='7'/>
+                    </colgroup> 
+                    <thead>
 
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
             </div>
         )
     }
