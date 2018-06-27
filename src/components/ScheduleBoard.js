7e070431a30805a9153d312939b22f22f1019826
Dasol-Jung <xenosophian@gmail.com>
2018-06-27 20:36:32 +0900

    set ScheduleBoard.js as a class component

---
+++ src/components/ScheduleBoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ScheduleBoard extends React.Component{ 
+    
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+};
+
+export default ScheduleBoard;
