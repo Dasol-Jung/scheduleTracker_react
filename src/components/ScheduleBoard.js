6b31ffa6db6a8b489d19556111b4d514b09ba8f8
Dasol-Jung <xenosophian@gmail.com>
2018-06-27 21:10:06 +0900

    created table head columns for Name, Rank, Section, and days

--- src/components/ScheduleBoard.js
+++ src/components/ScheduleBoard.js
@@ -11,7 +11,18 @@ class ScheduleBoard extends React.Component{
                         <col className='col2' span='7'/>
                     </colgroup> 
                     <thead>
-
+                        <tr>
+                            <th>Name</th>
+                            <th>Rank</th>
+                            <th>Section</th>
+                            <th>Mon</th>
+                            <th>Tue</th>
+                            <th>Wed</th>
+                            <th>Thu</th>
+                            <th>Fri</th>
+                            <th>Sat</th>
+                            <th>Sun</th>
+                        </tr> 
                     </thead>
                     <tbody>
 
