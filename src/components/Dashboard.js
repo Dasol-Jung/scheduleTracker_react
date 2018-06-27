a1e6467824e420d4ae2aeb18f4833ca66c653a39
Dasol-Jung <xenosophian@gmail.com>
2018-06-27 20:56:07 +0900

    added a change-handler in the week select element

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,11 +5,11 @@ const Dashboard = (props) => {
   return (
 
         <div>
-                <select>
-                <option>Weeks</option>
-                <option>Week 1</option>
-                <option>Week 2</option>
-                <option>Week 3</option>
+                <select onChange={props.weekChangeHandler}>
+                    <option>Weeks</option>
+                    <option>Week 1</option>
+                    <option>Week 2</option>
+                    <option>Week 3</option>
                 </select>
         </div>
   );
